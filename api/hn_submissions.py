0f20735675e82c4cebd17d18639758c5fcb3c258
cgutierrez1145 <70458522+cgutierrez1145@users.noreply.github.com>
2021-11-16 12:49:00 -0700

    Added commenting

--- api/hn_submissions.py
+++ api/hn_submissions.py
@@ -1,34 +1,57 @@
 from operator import itemgetter
 
-import requests
+import requests # HTTP Library
 
 # Make an API call and store the response.
 url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
-r = requests.get(url)
+
+ # assign request.Response object from URL to r
+r = requests.get(url) 
+
+# display stqatus code of HTTP connection
 print(f"Status code: {r.status_code}")
 
-# Process information about each submission.
-submission_ids = r.json()
-submission_dicts = []
+# # Process information about each submission.
+
+# JSON object representing request response data
+submission_ids = r.json() 
+
+submission_dicts = [] # list of submission in r (JSON object)
+
 for submission_id in submission_ids[:10]:
     # Make a separate API call for each submission.
+
+    # use f-string to parse url while inserting each sequential submission_id
     url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"
-    r = requests.get(url)
+
+    r = requests.get(url) # access new URL ass ign return object to r
+
+    # display stqatus code of HTTP connection
     print(f"id: {submission_id}\tstatus: {r.status_code}")
-    response_dict = r.json()
+
+    response_dict = r.json() # assign request.Response object from URL to r
     
     # Build a dictionary for each article.
     submission_dict = {
+
         'title': response_dict['title'],
+
         'hn_link': f"http://news.ycombinator.com/item?id={submission_id}",
+
         'comments': response_dict['descendants'],
     }
+
     submission_dicts.append(submission_dict)
     
 submission_dicts = sorted(submission_dicts, key=itemgetter('comments'),
                             reverse=True)
 
 for submission_dict in submission_dicts:
+
     print(f"\nTitle: {submission_dict['title']}")
+
     print(f"Discussion link: {submission_dict['hn_link']}")
+
     print(f"Comments: {submission_dict['comments']}")
+
+
